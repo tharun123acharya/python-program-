f41ff93bfd53cc6617a6b7633f5ecd725dd018ba
tharun <tharun.123acharya@gmail.com>
2026-04-30 07:58:18 +0530

    Add files via upload

---
+++ parametersandreturnvaluc.py
@@ -0,0 +1,8 @@
+add(num1,num2)
+c=num1+num2
+print("the addition is done")
+return c
+a=10
+b=20
+add(a,b)
+print(c)
---
+++ stringandsubstring.py
@@ -0,0 +1,6 @@
+s1=input("Enter first String : ");
+s2=input("Enter second String : ");
+print("First string is : ",s1);
+print("Second string is : ",s2);
+print("concatenations of two stings is : ",s1+s2);
+print("Substring of given string :",s1[1:4]);
